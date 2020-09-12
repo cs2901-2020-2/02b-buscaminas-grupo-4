7bfa2a316dd8568e74743059497f4767e29813fe
Carlos Reátegui <39816970+CarlosReategui@users.noreply.github.com>
2020-09-11 22:06:40 -0500

    variables de la clase y constructores

--- src/main/java/MineSweeper.java
+++ src/main/java/MineSweeper.java
@@ -1,9 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
-import java.lang.Math; 
+import java.util.Random;
+import java.util.Scanner;
 
 public class MineSweeper{
 
+    public ArrayList<ArrayList<Integer>> board = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<ArrayList<Boolean>> flags = new ArrayList<ArrayList<Boolean>>();
+
+    private int n;
+    boolean gameOver = false;
+
+    static final Logger logger = Logger.getLogger(MineSweeper.class.getName());
+
+    public MineSweeper(int _n) {
+        n = _n;
+        createGrid();
+        insertMines();
+    }
+
+    public MineSweeper(ArrayList<ArrayList<Integer>> newBoard) {
+        board = newBoard;
+    }
    
 }
