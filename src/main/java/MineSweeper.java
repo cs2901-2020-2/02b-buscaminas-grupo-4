3b80d35ecba93c88d5d5f07e0e8b6b2564b5cb1c
Fabrizio Franco A <39867790+fabriziofranco@users.noreply.github.com>
2020-09-11 22:09:00 -0500

    Update MineSweeper.java

--- src/main/java/MineSweeper.java
+++ src/main/java/MineSweeper.java
@@ -24,4 +24,26 @@ public class MineSweeper{
         board = newBoard;
     }
    
+    public void createGrid() {
+        for (int i = 0; i < n; i++) {
+            board.add(new ArrayList<Integer>());
+            flags.add(new ArrayList<Boolean>());
+            for (int j = 0; j < n; j++) {
+                board.get(i).add(j, 0);
+                flags.get(i).add(j, false);
+          }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = flags.get(i).get(j);
+                System.out.print(flag || gameOver ? board.get(i).get(j) + " ": "X" + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    
 }
